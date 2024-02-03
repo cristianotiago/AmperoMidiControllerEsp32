c1d72d94b61a1174709a9e80d7a87fd38a9efd65
Cristiano Tiago Modesto <cristianoufo@users.noreply.github.com>
2024-02-03 13:46:14 -0300

    Update AmperoMidiControllerEsp32.ino
    Refactor

--- AmperoMidiControllerEsp32.ino
+++ AmperoMidiControllerEsp32.ino
@@ -5,7 +5,7 @@
 int lcdColumns = 20;
 int lcdRows = 4;
 
-int expPin1 = 0;
+int expPin1 = 34;
 int currentVal1 = 0;
 int lastVal1 = 0;
 boolean afinadorMode = false;
@@ -34,7 +34,7 @@ boolean btnB5 = true;
 boolean btnB6 = true;
 
 
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 // 48 ate 53 A   63 on 64 off
@@ -56,23 +56,55 @@ OneButton button3(5, true, true);
 OneButton button4(17, true, true);
 OneButton button5(16, true, true);
 OneButton button6(4, true, true);
-OneButton button7(2, true, true);
-OneButton button8(15, true, true);
+OneButton button7(0, true, true);
+OneButton button8(2, true, true);
 
 #include "ModoStomp.h"
 
-void setup(){
- 
-  lcd.init();                    
+void setup() {
+
+  lcd.init();
   lcd.backlight();
   lcd.clear();
   MIDI.begin();
-  
+
+  // button 1
+  button1.attachClick(button1Press);
+  button1.attachLongPressStart(button1LongPressStart);
+
+  // button 2
+  button2.attachClick(button2Press);
+  button2.attachLongPressStart(button2LongPressStart);
+
+  // button 3
+  button3.attachClick(button3Press);
+  button3.attachLongPressStart(button3LongPressStart);
+
+  // button 4
+  button4.attachClick(button4Press);
+  button4.attachLongPressStart(button4LongPressStart);
+
+  // button 5
+  button5.attachClick(button5Press);
+  button5.attachLongPressStart(button5LongPressStart);
+
+  // button 6
+  button6.attachClick(button6Press);
+  button6.attachLongPressStart(button6LongPressStart);
+
+  // button 7
+  button7.attachClick(button7Press);
+  button7.attachLongPressStart(button7LongPressStart);
+
+  // button 8
+  button8.attachClick(button8Press);
+  button8.attachLongPressStart(button8LongPressStart);
+
   seletorStomp(true, true, true, true, true, true, true, true, true, true, true, true, true);
 }
 
 
-void loop(){
+void loop() {
   /*
   // set cursor to first column, first row
   lcd.setCursor(0, 0);
@@ -87,6 +119,15 @@ void loop(){
   delay(1000);
   lcd.clear(); 
   */
+  button1.tick();
+  button2.tick();
+  button3.tick();
+  button4.tick();
+  button5.tick();
+  button6.tick();
+  button7.tick();
+  button8.tick();
+  pedalExpressao();
 }
 
 
@@ -114,4 +155,222 @@ void pedalExpressao() {
 
   lastVal1 = currentVal1;
   delay(5);
+}
+
+
+
+void button1Press() {
+  //banco para cima
+  lcd.clear();
+
+  //writeBigString("  B A N K", 0, 0);
+  //writeBigString("      U P", 0, 2);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("BANK UP");
+    enviarControlChange(27, 0, 1);
+}
+
+void button1LongPressStart() {
+  if (naoEhAouB == true) {
+    naoEhAouB = false;
+    seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+  } else {
+    naoEhAouB = true;
+    seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+  }
+}
+
+
+void button2Press() {
+  //banco para baixo
+  lcd.clear();
+
+  // writeBigString("  B A N K", 0, 0);
+  // writeBigString("      D N", 0, 2);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("BANK DOWN");
+    enviarControlChange(26, 0, 1);
+}
+
+void button2LongPressStart() {
+  //afinador
+  if (afinadorMode == false) {
+    isAfinador(true);
+    lcd.clear();
+
+    lcd.setCursor(0, 1);
+    lcd.print("TUNER");
+    enviarControlChange(60, 64, 1);
+  } else if (afinadorMode == true) {
+    isAfinador(false);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("     ");
+    //writeBigString("         ", 0, 1);
+    seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    enviarControlChange(60, 63, 1);
+  }
+}
+
+void isAfinador(boolean valor) {
+  afinadorMode = valor;
+}
+//{48, 49, 50, 51, 52 , 53 }, //A1 A2 A3 A4 A5 A6
+//{54, 55, 56, 57, 58 , 59 }  //B1 B2 B3 B4 B4 B6
+
+void button3Press() {
+  if (naoEhAouB == true) {
+    if (btnA1 == true) {
+      btnA1 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnA1 == false) {
+      btnA1 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  } else if (naoEhAouB == false) {
+    if (btnB1 == true) {
+      btnB1 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnB1 == false) {
+      btnB1 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  }
+}
+
+void button3LongPressStart() {
+
+
+  if (stompMode == false) {
+    ativarStomp(true);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("STOMP");
+  } else if (stompMode == true) {
+    ativarStomp(false);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("PATCH");
+    //writeBigString("P A T C H", 0, 1);
+  }
+}
+
+void button4Press() {
+  if (naoEhAouB == true) {
+    if (btnA2 == true) {
+      btnA2 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnA2 == false) {
+      btnA2 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  } else if (naoEhAouB == false) {
+    if (btnB2 == true) {
+      btnB2 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnB2 == false) {
+      btnB2 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  }
+}
+
+void button4LongPressStart() {
+}
+
+
+void button5Press() {
+  if (naoEhAouB == true) {
+    if (btnA3 == true) {
+      btnA3 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnA3 == false) {
+      btnA3 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  } else if (naoEhAouB == false) {
+    if (btnB3 == true) {
+      btnB3 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnB3 == false) {
+      btnB3 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  }
+}
+
+void button5LongPressStart() {
+}
+
+void button6Press() {
+  if (naoEhAouB == true) {
+    if (btnA4 == true) {
+      btnA4 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnA4 == false) {
+      btnA4 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  } else if (naoEhAouB == false) {
+    if (btnB4 == true) {
+      btnB4 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnB4 == false) {
+      btnB4 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  }
+}
+
+void button6LongPressStart() {
+}
+
+
+void button7Press() {
+  if (naoEhAouB == true) {
+    if (btnA5 == true) {
+      btnA5 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnA5 == false) {
+      btnA5 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  } else if (naoEhAouB == false) {
+    if (btnB5 == true) {
+      btnB5 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnB5 == false) {
+      btnB5 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  }
+}
+
+void button7LongPressStart() {
+}
+
+void button8Press() {
+  if (naoEhAouB == true) {
+    if (btnA6 == true) {
+      btnA6 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnA6 == false) {
+      btnA6 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  } else if (naoEhAouB == false) {
+    if (btnB6 == true) {
+      btnB6 = false;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    } else if (btnB6 == false) {
+      btnB6 = true;
+      seletorStomp(btnA1, btnA2, btnA3, btnA4, btnA5, btnA6, btnB1, btnB2, btnB3, btnB4, btnB5, btnB6, naoEhAouB);
+    }
+  }
+}
+
+
+void button8LongPressStart() {
 }
